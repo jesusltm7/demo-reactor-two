ab64dd2e5b0e1d93e40cd06b5a37e17040fc9c55
jesus <jv.autentic.job@gmail.com>
2021-12-28 00:17:28 -0500

    Initial commit

---
+++ src/main/java/com/example/reactor/demoreactortwo/DemoReactorTwoApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactor.demoreactortwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoReactorTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoReactorTwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reactor/demoreactortwo/DemoReactorTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactor.demoreactortwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoReactorTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
